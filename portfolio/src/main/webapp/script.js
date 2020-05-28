7b41dd36d1b9438a435386dde210bb669625c2a0
Sara Qu <saraqu@college.harvard.edu>
2020-05-28 15:38:21 -0400

    Added side navbar and hobbies section as well as random image generator

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,12 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomImage() {
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  var randNum = Math.floor(Math.random() * 7) + 1;
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  var image = "/images/bake" + randNum + ".jpg";
+  document.getElementById('image-container').src = image;
 }
